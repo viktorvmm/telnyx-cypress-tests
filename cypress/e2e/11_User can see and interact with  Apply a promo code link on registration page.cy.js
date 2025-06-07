55487f187a2beb8831786489c2e933006756bbe4
Viktor Mironiuk <qa.viktormironiuk@gmail.com>
2025-06-07 20:17:28 +0300

    Fix: handle disabled promo code input in registration test

--- cypress/e2e/11_User can see and interact with  Apply a promo code link on registration page.cy.js
+++ cypress/e2e/11_User can see and interact with  Apply a promo code link on registration page.cy.js
@@ -2,41 +2,37 @@ describe('Registration Page - Additional Elements Check', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.closeCookiesBanner();
-    
+
     cy.get('a.c-bzrwjc-ikVhwtI-css[href="/sign-up"]', { timeout: 10000 })
       .should('be.visible')
       .click();
+
     cy.url().should('include', '/sign-up');
   });
 
   it('Перевірка наявності та роботи лінки "Apply a promo code"', () => {
-    // Перевірка наявності кнопки "Apply a promo code"
     cy.get('button.c-ewUecD', { timeout: 10000 })
       .should('be.visible')
       .and('contain.text', 'Apply a promo code')
-      .and('not.be.disabled');
+      .and('not.be.disabled')
+      .click();
 
-    // Клік на кнопку "Apply a promo code"
-    cy.get('button.c-ewUecD').click();
+    // Зачекаємо трохи, щоб DOM оновився (можна й без цього, якщо працює без)
+    cy.wait(500);
 
-    // Логування властивості disabled у поля для промокоду
-    cy.get('#promo_code').then($el => {
-      cy.log('Поле promo_code disabled:', $el.prop('disabled'));
-    });
-
-    // Перевірка появи поля для промокоду та його лейбла
+    // Заново отримуємо елемент після оновлення DOM
     cy.get('label[for="promo_code"]', { timeout: 10000 })
       .should('be.visible')
       .and('contain.text', 'Promo code');
 
-    // Перевірка тексту "Optional" у лейблі
     cy.get('label[for="promo_code"] em', { timeout: 10000 })
       .should('be.visible')
       .and('contain.text', 'Optional');
 
-    // Очікуємо, що поле стане активним (не disabled) перед введенням
+    // Отримуємо поле заново і перевіряємо + вводимо
     cy.get('#promo_code', { timeout: 10000 })
-      .should('be.visible')
+      .should('exist')
+      .and('be.visible')
       .and('not.be.disabled')
       .type('PROMO2025')
       .should('have.value', 'PROMO2025');
